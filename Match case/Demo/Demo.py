54a8d8f2d38ac70900dcf3cca51ec0c9c80716cf
Venkata Sreenivas <venkatasreenivas2006@gmail.com>
2026-01-23 19:28:46 +0530

    Add files via upload

---
+++ Match case/Demo/Demo.py
@@ -0,0 +1,29 @@
+''' match... case
+===================
+
+1. It is and alternative for if.... elif....
+
+2. In other programming language there is switch..case
+
+3. In python there is no switch... case upto python 3.9 version but from python 3.10
+onwards there is a concept is introduced in python i.e match...case it will work same
+as switch..case
+
+4. It is faster than if..elif...
+
+5. It is used to develop menu driven programms
+
+syn:
+match variable:
+... case values:
+    ....stmt(s)
+
+    case values:
+    .... stmt(s)
+
+    case value:
+    ....stmt(s)
+
+    case default:
+    ....stmt(s)
+'''
---
+++ Match case/Demo/EX1.py
@@ -0,0 +1,12 @@
+n=int(input("Enter a number: "))
+
+match n:
+    case 1:
+        print("One")
+    case 2:
+        print("Two")
+    case 3:
+        print("Three")
+    case default:
+        print("Default")
+
