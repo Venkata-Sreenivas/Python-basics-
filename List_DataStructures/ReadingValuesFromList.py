aba7a3e4a5056c8eb4843936f2e33b3387f2a344
Venkata Sreenivas <venkatasreenivas2006@gmail.com>
2026-01-30 19:06:54 +0530

    Add files via upload

---
+++ List_DataStructures/ReadingValuesFromList.py
@@ -0,0 +1,22 @@
+'''To print Possible Values from list'''
+
+#Indexing
+
+lst=[10,20,30,40,50,60]
+
+print("List: ",lst)
+print("3rd Object: ",lst[2])
+print("3rd Object: ", lst[-4])
+
+print("=================================")
+
+#Slicing
+print("First 3 objects: ",lst[0:3:1])
+print("Last 3 Objects: ",lst[3:6:1])
+print("Last 3 Objects in reverse: ",lst[-1:-4:-1])
+print("=====================================")
+
+#Unpacking
+#Unpacking Using Slicing
+#While
+#For
