8c2a4bb5d2eddd223200918296e0e88693cc424d
Venkata Sreenivas <venkatasreenivas2006@gmail.com>
2026-01-31 21:34:40 +0530

    Add files via upload

---
+++ SingleLoopTask2.py
@@ -0,0 +1,15 @@
+'''Using Single loop'''
+
+i= 1
+while i <= 5:
+    if i == 1:
+        print("1   5")
+    elif i == 2:
+        print("2   4   1   5")
+    elif i == 3:
+        print("3   3   sai   kphb")
+    elif i == 4:
+        print("4   2   3")
+    elif i == 5:
+        print("5   1   sai   kphb")
+    i += 1
