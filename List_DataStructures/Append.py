b3f2de010d4145183ca04eb00a431c21af198c44
Venkata Sreenivas <venkatasreenivas2006@gmail.com>
2026-02-02 20:29:52 +0530

    Add files via upload

---
+++ List_DataStructures/Append.py
@@ -0,0 +1,9 @@
+'''List class objesct reference L.append(item)
+Adding an item to the list collection dynamically'''
+
+lst=[]
+for i in range(1,4):
+    item=input("Enter and item: ")
+    lst.append(item)
+    
+print("List: ",lst)
