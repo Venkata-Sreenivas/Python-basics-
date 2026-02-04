2e39dd9c12107faa017f2de45dc1791c7daff62c
Venkata Sreenivas <venkatasreenivas2006@gmail.com>
2026-02-04 17:47:08 +0530

    Add files via upload

---
+++ Tuple_DataStructure/Unpacking.py
@@ -0,0 +1,16 @@
+
+t=(10,"Sreenivas","Hyd")
+print("Tuple is: ",t)
+
+#Unpacking
+a,b,c=t
+
+print(a,b,c,sep=',')
+#Unpacking using slicing
+t=(10,"Sreenivas","Hyd",40,50,60)
+print("Tuple is: ",t)
+m1,m2,m3=t[3:6:1]
+print(m1,m2,m3,sep=',')
+print("=============================")
+#while
+#for
