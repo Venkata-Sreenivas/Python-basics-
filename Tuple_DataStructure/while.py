4ca51f4d222575abd6b7c9c12a42a2a847b65e8c
Venkata Sreenivas <venkatasreenivas2006@gmail.com>
2026-02-04 17:47:32 +0530

    Add files via upload

---
+++ Tuple_DataStructure/while.py
@@ -0,0 +1,13 @@
+t=(10,"Sreenivas","Hyd")
+print("Tuple is: ",t)
+
+#while
+index=0
+while index<len(t):
+    print(t[index])
+    index=index+1
+print("====================")
+
+#for
+for i in t:
+    print(i)
